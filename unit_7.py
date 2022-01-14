9eb5af54faf95ae90978be2f7e1ea951ec2724cd
fLanc3GV <46271420+Flance-s@users.noreply.github.com>
2022-01-14 15:59:24 -0600

    finished circle radius and started xy intercept solver

--- unit_7.py
+++ unit_7.py
@@ -49,6 +49,8 @@ def solve_squares():
 
             if c % a != 0:
                 i = i * -1
+                print("\nnon-Simplified Radical")
+                print("X = " + str(i) + "+-sqrt(" + str(g) + "/" + str(a) + ")")
                 g = g * a
                 print("\nbaDon\'t forget to simplify the radical!")
                 print("X = " + str(i) + "+-sqrt(" + str(g) + ")/" + str(a) + "")
@@ -66,11 +68,66 @@ ax^2 + bx + c = 0""")
         main()
     else:
         solve_squares()
-    
 
+def circlerad_solve():
+    print("\n1. Solve")
+    print("2. Show Example")
+    print("0. Exit\n")
+    crs_menu = input("Pick Mode: ")
+
+    if crs_menu == '1':
+        print("Circle Side")
+        a = input("x: ")
+        b = input("y: ")
+        print("Circle Center")
+        c = input("h: ")
+        d = input("k: ")
+
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        d = float(d)
+
+        ans = pow((a - c), 2) + pow((b - d), 2)
+        print("\nRadius without sqrt: sqrt(" + str(ans) + ")")
+        ans = math.sqrt(ans)
+        print("Radius with sqrt: " + str(ans))
+        
+        
+        circlerad_solve()
+    elif crs_menu == '2':
+        print("""
+(x - h)^2 + (y - k)^2 = r^2""")
+        circlerad_solve()
+    elif crs_menu == '0':
+        main()
+    else:
+        circlerad_solve()
+
+def x_y_intercepts():
+    print("\n1. Solve")
+    print("2. Show Example")
+    print("0. Exit\n")
+    xyi_menu = input("Pick Mode: ")
+
+    if xyi_menu == '1':
+        print("hello")
+        x_y_intercept()
+    elif xyi_menu == '2':
+        print("""
+""")
+        x_y_intercepts()
+    elif xyi_menu == '0':
+        main()
+    else:
+        x_y_intercepts()
+    
 def main():
     #  Modes
+    print("Unit 7.1")
     print("1. Solving for squares")
+    print("2. Solve for Circle radius")
+    print("3. Solve for circle x,y intercepts")
 
     print("\n0. Exit")
 
@@ -80,6 +137,10 @@ def main():
         exit()
     elif mode == '1':
         solve_squares()
+    elif mode == '2':
+        circlerad_solve()
+    elif mode == '3':
+        x_y_intercepts()
     else:
         print("\nYou didn\'t choose a program, Resetting...\n")
         main()
