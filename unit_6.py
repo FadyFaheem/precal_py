37d68f65473234358d57d762dedb367674bce1e8
Flance <46271420+Flance-s@users.noreply.github.com>
2022-01-14 20:34:13 -0600

    due to calc limits making programs smaller

--- unit_6.py
+++ unit_6.py
@@ -1,17 +1,14 @@
 import math
 
 def side_to_angle():
-
     print("\n1. Solve")
     print ("2. See possible equations")
     print ("0. Go back to Main Menu\n")
     menu = input("Pick mode:")
-
     if menu == '1':
         a = input("\nSide 1: ")
         b = input("Side 2: ")
         c = input("Side 3: ")
-
         top = pow(float(a),2) + pow(float(b),2) - pow(float(c),2)
         bottom = 2 * float(a) * float(b)
         answer = math.degrees(math.acos(float(top) / float(bottom)))
@@ -35,21 +32,17 @@ a^2 + b^2 - c^2
         main()
     else:
         side_to_angle()
-
 def side_solve():
     print("\n1. Solve")
     print("2. See possible equations")
     print("0. Go back to Main Menu\n")
     side_menu = input("Pick mode: ")
-
     if side_menu == '1':
         a = input("\nSide 1: ")
         b = input("Side 2: ")
         c = input("Angle 3: ")
-
         answer = math.sqrt(pow(float(a),2) + pow(float(b),2) - 2 * (float(a) * float(b)) * math.cos(math.radians(float(c))))
         print("Angle Length: " + str(answer))
-
         side_solve()
     elif side_menu == '2':
         print("""
@@ -65,19 +58,15 @@ c^2 = a^2 + b^2 - 2ab * cosC""")
         main()
     else:
         side_solve()
-
 def area_triangle():
     print("\n1. Solve")
     print("2. See possible equations")
     print("0. Go back to Main Menu\n")
     area_menu = input("Pick mode: ")
-
     if area_menu == '1':
-        
         a = input("\nSide 1:")
         b = input("Side 2:")
         c = input("Angle 3:")
-
         answer = (1 / 2) * float(a) * float(b) * math.sin(math.radians(float(c)))
         print("Area: " + str(answer) + "m^2")
         area_triangle()
@@ -99,20 +88,16 @@ area = - * ac * SinB
         main()
     else:
         area_triangle()
-        
 def minutes_decimals():
     print("\n1. Solve")
     print("2. See example")
     print("0. Go back to Main Menu\n")
     minutes_menu = input("Pick mode: ")
-
     if minutes_menu == '1':
         a = input("\nDegrees°: ")
         b = input("Minutes\': ")
-
         c = float(a) + (float(b) / 60)
         d = (float(a) * 60) + float(b)
-
         print("\nTotal Degrees: " + str(c) + "°")
         print("Total Minutes: " + str(d) + "\'")
         print("Total Seconds: " + str(d * 60) + "sec")
@@ -124,19 +109,16 @@ def minutes_decimals():
         main()
     else:
         minutes_decimals()
-
 def sin_side():
     print("\n1. Solve")
     print("2. See possible equations")
     print("0. Go back to Main Menu\n")
     sin_menu = input("Pick mode: ")
-
     if sin_menu == '1':
         print("Angle 1 is the side you\'re solving for")
         a = input("Angle 1: ")
         b = input("Side 2: ")
         c = input("Angle 2: ")
-
         answer = float(b) * math.sin(math.radians(float(a))) / math.sin(math.radians(float(c)))
         print("Side 1: " + str(answer))
         sin_side()
@@ -158,31 +140,25 @@ sinA   sinC""")
         main()
     else:
         sin_side()
-
 def comp_form():
     print("\n1. Solve")
     print("2. See example")
     print("0. Go back to Main Menu\n")
     comp_menu = input("Pick mode: ")
-
     if comp_menu == '1':
         a = input("\nx1: ")
         b = input("y1: ")
         c = input("x2: ")
         d = input("y2: ")
-
         e = float(c) - float(a) # X
         f = float(d) - float(b) # Y
-        
         g = math.sqrt(pow(e, 2) + pow(f, 2))
         h = pow(e, 2) + pow(f, 2)
         i = 0
-        
         if e > 0 and f < 0:
             i = 360
         elif e < 0 and f > 0 or e < 0 and f < 0:
             i = 180
-        
         l = round(i + math.degrees(math.atan(f/e)), 0)
         print("v1 = " + str(e) + "\nv2 = " + str(f) + "\n<" + str(e) + ", " + str(f) + ">")
         print("Only use magnituide if whole number!")
@@ -197,13 +173,11 @@ def comp_form():
         main()
     else:
         comp_form()
-
 def comp_form_multiple():
     print("\n1. Solve")
     print("2. See possible equations")
     print("0. Go back to Main Menu\n")
     compmulti_menu = input("Pick mode: ")
-
     if compmulti_menu == '1':
         print("if a negative is present, don\'t forget it!")
         a = input("a: ")
@@ -212,14 +186,9 @@ def comp_form_multiple():
         d = input("b: ")
         e = input("x2: ")
         f = input("y2: ")
-
         g = int(d) * int(e) + int(a) * int(b)
         h = int(d) * int(f) + int(a) * int(c)
-
-
         print("v1 = " + str(g) + "\nv2 = " + str(h) + "\n<" + str(g) + ", " + str(h) + ">")
-        
-        
         comp_form_multiple()
     elif compmulti_menu == '2':
         print("""
@@ -229,13 +198,11 @@ a(x1, y2) - b(x2, y2) = <v1, v2>""")
         main()
     else:
         comp_form_multiple()
-
 def direc_mag():
     print("\n1. Solve")
     print("2. See possible equations")
     print("0. Go back to Main Menu\n")
     mag_menu = input("Pick mode: ")
-
     if mag_menu == '1':
         a = input("m1: ")
         b = input("X1: ")
@@ -243,26 +210,21 @@ def direc_mag():
         d = input("m2: ")
         e = input("X2: ")
         f = input("Y2: ")
-        
         g = (float(a) *  math.cos(math.radians(float(b)))) + (float(d) * math.cos(math.radians(float(e)))) # X
         h = (float(a) * math.sin(math.radians(float(c)))) + (float(d) * math.sin(math.radians(float(f)))) # Y
         i = math.sqrt(pow(g, 2) + pow(h, 2)) 
         j = math.degrees(math.atan(h/g))
         k = 0 
-        
         if h < 0 and g > 0:
             k = 360
         elif h < 0 and g < 0 or h < 0 and g > 0:
             k = 180
-        
         l = round(k + j)
-        
         print("\nx = " + str(g))
         print("y = " + str(h))
         print("Magnituide: " + str(i))
         print("Degree: " + str(l))
         print("Mag and Dir Form: " + str(round(i, 1)) + "<cos" + str(l) + "°, sin" + str(l) + "°>")
-        
         direc_mag()
     elif mag_menu == '2':
         print("""
@@ -272,21 +234,17 @@ m * <cosV1, sinV2> = m1 * <cosX1, sinY1> + m2 * <cosX2, sinY1>""")
         main()
     else:
         direc_mag()
-    
 def singlecomp_form():
     print("\n1. Solve")
     print("2. See example")
     print("0. Go back to Main Menu\n")
     singlecomp_menu = input("Pick mode: ")
-
     if singlecomp_menu == '1':
         a = input("\nm: ")
         b = input("x1: ")
         c = input("y1: ")
-        
         f = float(a) * float(b) # X
         g = float(a) * float(c) # Y
-        
         print("<" + str(f) +  ", " + str(g) + ">")
         singlecomp_form()
     elif singlecomp_menu == '2':
@@ -297,22 +255,18 @@ def singlecomp_form():
         main()
     else: 
         singlecomp_form()
-   
 def solve_vec():
     print("\n1. Solve")
     print("2. See example")
     print("0. Go back to Main Menu\n")
     vec_menu = input("Pick mode: ")
-
     if vec_menu == '1':
         a = input("\nx1: ")
         b = input("y1: ")
         c = input("x2: ")
         d = input("y2: ")
-
         e = float(c) + float(a) # X
         f = float(d) + float(b) # Y
-        
         print("\nX = " + str(e))
         print("Y = " + str(f))
         solve_vec()
@@ -324,8 +278,6 @@ def solve_vec():
         main()
     else: 
         solve_vec()
-    
-    
 def main():
     print("\nLaw of Cosines")
     print("1. Side to Degrees (SSS)")
--- unit_7_1.py
+++ unit_7_1.py
@@ -1,68 +1,69 @@
 import math
 
 def solve_squares():
-    print("\n1. Solve")
+    print("1. Solve")
     print("2. Show Example")
-    print("0. Exit\n")
+    print("0. Exit")
     ss_menu = input("Pick Mode: ")
 
     if ss_menu == '1':
-        a = input("\na: ")
+        a = input("a: ")
         b = input("b: ")
         c = input("c: ")
-
+        
         a = float(a)
         b = float(b)
         c = float(c)
-
-        if b == '0':
-            c = float(c) * -1
-            print("\nx = sqrt(" + str(c) + ")")
-        elif a == 1 or a == -1:
-            e = a
-            f = b
-            g = c * -1
-            
-            h = pow((f * 0.5), 2)
-            j = math.sqrt(h)
-            
-            i = g + h
-            
-            print("\naDon\'t forget to simplify the radical!")
-            print("X = " + str(j) + "+-sqrt(" + str(i) + ")")
-        elif a != '1':
+        
+        if b == 0:
+            c = c * -1
+            print("x = +-sqrt(" + str(c) + ")")
+        if a != 1:
             e = a / a
-
+            
             if b % a == 0:
                 f = b / a
-            else:
-                f = float(b)
+            else: 
+                f = b
+                
+            h = math.pow((f * .5), 2)
 
-            h = pow(f * 0.5, 2)
-            
             if c % a == 0:
-                g = ((c * -1) / a)  + h 
+                g = ((c / a) * -1) + h 
             else:
-                g = (float(c) * -1) + (h * a)
-
-            i = math.sqrt(h) * -1
-
-            if c % a != 0:
-                i = i * -1
-                print("\nnon-Simplified Radical")
-                print("X = " + str(i) + "+-sqrt(" + str(g) + "/" + str(a) + ")")
-                g = g * a
-                print("\nbaDon\'t forget to simplify the radical!")
-                print("X = " + str(i) + "+-sqrt(" + str(g) + ")/" + str(a) + "")
-            else:
-                print("\ncDon\'t forget to simplify the radical!")
-                print("X = " + str(i) + "+-sqrt(" + str(g) + ")")
-
+                g = (c * -1) + (h * a)
         
+            i = math.sqrt(h)
+            if f < 0:
+                if c % a == 0:
+                    print("x = " + str(i) + "+-sqrt(" + str(g) + ")")
+                else:
+                    print("x = " + str(i) + "+-sqrt(" + str(g) + "/" + str(a) + ")")
+                    print("After simplifying")
+                    g = g * a
+                    print("x = " + str(i) + "+-sqrt(" + str(g) + ")/" + str(a))
+            else:
+                i = i * -1
+                if c % a == 0:
+                    print("x = " + str(i) + "+-sqrt(" + str(g) + ")")
+                else:
+                    print("x = " + str(i) + "+-sqrt(" + str(g) + "/" + str(a) + ")")
+                    print("After simplifying")
+                    g = g * a
+                    print("x = " + str(i) + "+-sqrt(" + str(g) + ")/" + str(a))
+        if a == 1:
+            e = a
+            f = b
+            g = (c * -1) + (math.pow((f * .5), 2))
+            h = math.sqrt(math.pow((f * .5), 2))
+            if f > 0:
+                h = h * -1
+                print("x = " + str(h) + "+-sqrt(" + str(g) + ")")
+            else:
+                print("x = " + str(h) + "+-sqrt(" + str(g) + ")")
         solve_squares()
     elif ss_menu == '2':
-        print("""
-ax^2 + bx + c = 0""")
+        print("""ax^2 + bx + c = 0""")
         solve_squares()
     elif ss_menu == '0':
         main()
@@ -70,9 +71,9 @@ ax^2 + bx + c = 0""")
         solve_squares()
 
 def circlerad_solve():
-    print("\n1. Solve")
+    print("1. Solve")
     print("2. Show Example")
-    print("0. Exit\n")
+    print("0. Exit")
     crs_menu = input("Pick Mode: ")
 
     if crs_menu == '1':
@@ -89,15 +90,14 @@ def circlerad_solve():
         d = float(d)
 
         ans = pow((a - c), 2) + pow((b - d), 2)
-        print("\nRadius without sqrt: sqrt(" + str(ans) + ")")
+        print("Radius without sqrt: sqrt(" + str(ans) + ")")
         ans = math.sqrt(ans)
         print("Radius with sqrt: " + str(ans))
         
         
         circlerad_solve()
     elif crs_menu == '2':
-        print("""
-(x - h)^2 + (y - k)^2 = r^2""")
+        print("""(x - h)^2 + (y - k)^2 = r^2""")
         circlerad_solve()
     elif crs_menu == '0':
         main()
@@ -105,9 +105,9 @@ def circlerad_solve():
         circlerad_solve()
 
 def x_y_intercepts():
-    print("\n1. Solve")
+    print("1. Solve")
     print("2. Show Example")
-    print("0. Exit\n")
+    print("0. Exit")
     xyi_menu = input("Pick Mode: ")
 
     if xyi_menu == '1':
@@ -129,9 +129,9 @@ def main():
     print("2. Solve for Circle radius")
     print("3. Solve for circle x,y intercepts")
 
-    print("\n0. Exit")
+    print("0. Exit")
 
-    mode = input("\nPick Mode: ")
+    mode = input("Pick Mode: ")
 
     if mode == '0':
         exit()
@@ -142,7 +142,7 @@ def main():
     elif mode == '3':
         x_y_intercepts()
     else:
-        print("\nYou didn\'t choose a program, Resetting...\n")
+        print("You didn\'t choose a program, Resetting...")
         main()
 
 main()
